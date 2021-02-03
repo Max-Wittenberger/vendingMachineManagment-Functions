aa79597d7c0ce97eef97029ac35654d5ed12996f
Max-Wittenberger <max@statpile.de>
2021-02-03 11:59:36 +0100

    refacrtoring

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBXYJGrIaWKlZoL-XXrPMXCJ86WNkw6oXQ",
+  authDomain: "vendingmachinemanagement.firebaseapp.com",
+  projectId: "vendingmachinemanagement",
+  storageBucket: "vendingmachinemanagement.appspot.com",
+  messagingSenderId: "259585982789",
+  appId: "1:259585982789:web:abc8606a79fd834454be2c",
+  measurementId: "G-YPNY8MPP0P"
+};
+
