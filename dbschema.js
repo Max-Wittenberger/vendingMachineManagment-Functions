1de566ebf014bf9a9b1a86958e8c9579db80d8d9
Max-Wittenberger <max@statpile.de>
2021-02-03 09:32:00 +0100

    Initial commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2021-02-02T20:07:57.327Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
